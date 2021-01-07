2ea0870a7367a517632d02580fd3c6b040916033
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-01-07 18:18:56 -0500

    Add route creater

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import fse from 'fs-extra'
 import { execSync } from 'child_process'
 import packageTemplace from "../lib/package.js";
 import appTemplate from "../lib/app.js";
+import getTemplate from '../lib/getTemplate.js';
 import { writeFile } from "./fs/writeFile.js";
 import {getFileList} from "./util/sourceEnlister.js";
 
 (async() => {
     await writeFile("package.json", JSON.stringify(packageTemplace))
     await writeFile("app.js", appTemplate)
-    execSync("npm install")
-    execSync("npm run start")
     const list = await getFileList()
+    for (const route of list) {
+        fs.appendFile("app.js", getTemplate.replace(new RegExp(/ROUTE_HERE/g), route), () => {
+            if (route === list[list.length - 1]) {
+                execSync("npm install")
+                execSync("npm run start")
+                console.log("finish")
+            }
+        })
+    }
 })()
